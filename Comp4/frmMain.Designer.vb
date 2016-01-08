b994a618dca5703d7b43df3d602494c3f094771e
millpond <98hamiltonben@gmail.com>
2016-01-08 09:49:34 +0000

    bleh

--- Comp4/frmMain.Designer.vb
+++ Comp4/frmMain.Designer.vb
@@ -22,175 +22,56 @@ Partial Class frmMain
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.txtUID = New System.Windows.Forms.TextBox()
-        Me.txtLogin = New System.Windows.Forms.TextBox()
-        Me.txtPassword = New System.Windows.Forms.TextBox()
-        Me.txtEmail = New System.Windows.Forms.TextBox()
-        Me.txtForename = New System.Windows.Forms.TextBox()
-        Me.txtSurname = New System.Windows.Forms.TextBox()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.Label3 = New System.Windows.Forms.Label()
-        Me.Label4 = New System.Windows.Forms.Label()
-        Me.Label5 = New System.Windows.Forms.Label()
-        Me.Label6 = New System.Windows.Forms.Label()
-        Me.btnAddtoDb = New System.Windows.Forms.Button()
-        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.TabControl1.SuspendLayout()
         Me.SuspendLayout()
         '
-        'txtUID
+        'TabControl1
         '
-        Me.txtUID.Location = New System.Drawing.Point(139, 12)
-        Me.txtUID.Name = "txtUID"
-        Me.txtUID.Size = New System.Drawing.Size(223, 20)
-        Me.txtUID.TabIndex = 0
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Location = New System.Drawing.Point(12, 12)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(646, 414)
+        Me.TabControl1.TabIndex = 0
         '
-        'txtLogin
+        'TabPage1
         '
-        Me.txtLogin.Location = New System.Drawing.Point(139, 38)
-        Me.txtLogin.Name = "txtLogin"
-        Me.txtLogin.Size = New System.Drawing.Size(223, 20)
-        Me.txtLogin.TabIndex = 1
+        Me.TabPage1.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(638, 388)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "TabPage1"
+        Me.TabPage1.UseVisualStyleBackColor = True
         '
-        'txtPassword
+        'TabPage2
         '
-        Me.txtPassword.Location = New System.Drawing.Point(139, 64)
-        Me.txtPassword.Name = "txtPassword"
-        Me.txtPassword.Size = New System.Drawing.Size(223, 20)
-        Me.txtPassword.TabIndex = 2
-        '
-        'txtEmail
-        '
-        Me.txtEmail.Location = New System.Drawing.Point(139, 90)
-        Me.txtEmail.Name = "txtEmail"
-        Me.txtEmail.Size = New System.Drawing.Size(223, 20)
-        Me.txtEmail.TabIndex = 3
-        '
-        'txtForename
-        '
-        Me.txtForename.Location = New System.Drawing.Point(139, 116)
-        Me.txtForename.Name = "txtForename"
-        Me.txtForename.Size = New System.Drawing.Size(223, 20)
-        Me.txtForename.TabIndex = 4
-        '
-        'txtSurname
-        '
-        Me.txtSurname.Location = New System.Drawing.Point(139, 142)
-        Me.txtSurname.Name = "txtSurname"
-        Me.txtSurname.Size = New System.Drawing.Size(223, 20)
-        Me.txtSurname.TabIndex = 5
-        '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(104, 15)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(29, 13)
-        Me.Label1.TabIndex = 6
-        Me.Label1.Text = "UID:"
-        '
-        'Label2
-        '
-        Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(97, 41)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(36, 13)
-        Me.Label2.TabIndex = 7
-        Me.Label2.Text = "Login:"
-        '
-        'Label3
-        '
-        Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(77, 67)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(56, 13)
-        Me.Label3.TabIndex = 8
-        Me.Label3.Text = "Password:"
-        '
-        'Label4
-        '
-        Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(96, 93)
-        Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(35, 13)
-        Me.Label4.TabIndex = 9
-        Me.Label4.Text = "Email:"
-        '
-        'Label5
-        '
-        Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(76, 119)
-        Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(57, 13)
-        Me.Label5.TabIndex = 10
-        Me.Label5.Text = "Forename:"
-        '
-        'Label6
-        '
-        Me.Label6.AutoSize = True
-        Me.Label6.Location = New System.Drawing.Point(81, 145)
-        Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(52, 13)
-        Me.Label6.TabIndex = 11
-        Me.Label6.Text = "Surname:"
-        '
-        'btnAddtoDb
-        '
-        Me.btnAddtoDb.Location = New System.Drawing.Point(194, 196)
-        Me.btnAddtoDb.Name = "btnAddtoDb"
-        Me.btnAddtoDb.Size = New System.Drawing.Size(75, 23)
-        Me.btnAddtoDb.TabIndex = 12
-        Me.btnAddtoDb.Text = "Add to DB"
-        Me.btnAddtoDb.UseVisualStyleBackColor = True
-        '
-        'DataGridView1
-        '
-        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Location = New System.Drawing.Point(337, 196)
-        Me.DataGridView1.Name = "DataGridView1"
-        Me.DataGridView1.Size = New System.Drawing.Size(310, 230)
-        Me.DataGridView1.TabIndex = 13
+        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(638, 388)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "TabPage2"
+        Me.TabPage2.UseVisualStyleBackColor = True
         '
         'frmMain
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(670, 438)
-        Me.Controls.Add(Me.DataGridView1)
-        Me.Controls.Add(Me.btnAddtoDb)
-        Me.Controls.Add(Me.Label6)
-        Me.Controls.Add(Me.Label5)
-        Me.Controls.Add(Me.Label4)
-        Me.Controls.Add(Me.Label3)
-        Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.txtSurname)
-        Me.Controls.Add(Me.txtForename)
-        Me.Controls.Add(Me.txtEmail)
-        Me.Controls.Add(Me.txtPassword)
-        Me.Controls.Add(Me.txtLogin)
-        Me.Controls.Add(Me.txtUID)
+        Me.Controls.Add(Me.TabControl1)
         Me.Name = "frmMain"
         Me.Text = "Main"
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.TabControl1.ResumeLayout(False)
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
+    Friend WithEvents TabControl1 As System.Windows.Forms.TabControl
+    Friend WithEvents TabPage1 As System.Windows.Forms.TabPage
+    Friend WithEvents TabPage2 As System.Windows.Forms.TabPage
 
-    Friend WithEvents txtUID As TextBox
-    Friend WithEvents txtLogin As TextBox
-    Friend WithEvents txtPassword As TextBox
-    Friend WithEvents txtEmail As TextBox
-    Friend WithEvents txtForename As TextBox
-    Friend WithEvents txtSurname As TextBox
-    Friend WithEvents Label1 As Label
-    Friend WithEvents Label2 As Label
-    Friend WithEvents Label3 As Label
-    Friend WithEvents Label4 As Label
-    Friend WithEvents Label5 As Label
-    Friend WithEvents Label6 As Label
-    Friend WithEvents btnAddtoDb As Button
-    Friend WithEvents DataGridView1 As DataGridView
 End Class
--- Comp4/frmMain.vb
+++ Comp4/frmMain.vb
@@ -1,40 +1,96 @@
 ﻿Public Class frmMain
-    Dim addUserConn As New PostgreSQL
-    Dim dt As New DataTable
+    Dim curWidth As Integer = Me.Width
+    Dim curHeight As Integer = Me.Height
+    Dim initWidth As Integer
+    Dim initHeight As Integer
 
-    Private Sub btnAddtoDb_Click(sender As Object, e As EventArgs) Handles btnAddtoDb.Click
-        Dim detailList As New List(Of Object)
-        Dim listList As New List(Of List(Of Object))
-        addUserConn.Database = "userDB" 'Ensure UserDB is selected
-        addUserConn.ConnectToDB()
-        detailList.Add(200)
-        detailList.Add(txtLogin.Text)
-        detailList.Add(txtPassword.Text)
-        detailList.Add(txtEmail.Text)
-        detailList.Add(txtForename.Text)
-        detailList.Add(txtSurname.Text)
-        For i = Int(txtUID.Text) To Int(txtUID.Text) + 10
-            detailList(0) = i
-            listList.Add(detailList.ToList)
+
+    Private Sub frmMain_Resize(sender As Object, e As EventArgs) Handles MyBase.Resize
+
+        Dim widthRatio As Double = (Me.Width - initWidth) / curWidth
+        Dim heightRatio As Double = (Me.Height - initHeight) / curHeight
+
+        For Each ctrl In Controls
+            ctrl.Width += CInt(ctrl.Width * widthRatio)
+            ctrl.Height += CInt(ctrl.Height * heightRatio)
+            ctrl.Left += CInt(ctrl.Left * widthRatio)
+            ctrl.Top += CInt(ctrl.Top * heightRatio)
         Next
-        addUserConn.AddToTable("users", listList)
-        refresh()
-        MsgBox("OK!")
+
+        curWidth = Me.Width
+        curHeight = Me.Height
+
     End Sub
 
     Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-        Dim addUserConn As New PostgreSQL
-        addUserConn.Database = "userDB" 'Ensure UserDB is selected
-        addUserConn.ConnectToDB()
-        addUserConn.UpdateTable("users", dt)
-        DataGridView1.DataSource = dt
+        initWidth = Me.Width
+        initHeight = Me.Height
     End Sub
 
-    Function refresh()
-        addUserConn.Database = "userDB" 'Ensure UserDB is selected
-        addUserConn.ConnectToDB()
-        addUserConn.UpdateTable("users", dt)
-        DataGridView1.DataSource = dt
-    End Function
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    'Dim addUserConn As New PostgreSQL
+    'Dim dt As New DataTable
+
+    'Private Sub btnAddtoDb_Click(sender As Object, e As EventArgs)
+    '    Dim detailList As New List(Of Object)
+    '    Dim listList As New List(Of List(Of Object))
+    '    addUserConn.Database = "userDB" 'Ensure UserDB is selected
+    '    addUserConn.ConnectToDB()
+    '    detailList.Add(200)
+    '    detailList.Add(txtLogin.Text)
+    '    detailList.Add(txtPassword.Text)
+    '    detailList.Add(txtEmail.Text)
+    '    detailList.Add(txtForename.Text)
+    '    detailList.Add(txtSurname.Text)
+    '    For i = Int(txtUID.Text) To Int(txtUID.Text) + 10
+    '        detailList(0) = i
+    '        listList.Add(detailList.ToList)
+    '    Next
+    '    addUserConn.AddToTable("users", listList)
+    '    refresh()
+    '    MsgBox("OK!")
+    'End Sub
+
+    'Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    '    Dim addUserConn As New PostgreSQL
+    '    addUserConn.Database = "userDB" 'Ensure UserDB is selected
+    '    addUserConn.ConnectToDB()
+    '    addUserConn.UpdateTable("users", dt)
+    '    DataGridView1.DataSource = dt
+    'End Sub
+
+    'Function refresh()
+    '    addUserConn.Database = "userDB" 'Ensure UserDB is selected
+    '    addUserConn.ConnectToDB()
+    '    addUserConn.UpdateTable("users", dt)
+    '    DataGridView1.DataSource = dt
+    'End Function
+
 End Class
